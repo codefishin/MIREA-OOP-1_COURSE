656ab69fa4fc68ed3cf67a13b578fe10be975681
stomp <150149808+stomp666@users.noreply.github.com>
2024-09-04 21:08:00 +0300

    Create cl_application.hpp

---
+++ Kursach2/cl_application.hpp
@@ -0,0 +1,17 @@
+#ifndef __CL_APPLICATION__H
+#define __CL_APPLICATION__H
+#include "class2.hpp"
+#include "class3.hpp"
+#include "class4.hpp"
+#include "class5.hpp"
+#include "class6.hpp"
+#include <algorithm>
+#include <math.h>
+class cl_application : public cl_base
+{
+   public:
+    cl_application(cl_base* headptr);
+    void build_tree_objects();
+    int exec_app();
+};
+#endif
