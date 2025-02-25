36532776e0f73d13ae52afef9456117a6075a81c
st <150149808+codefishin@users.noreply.github.com>
2025-02-26 00:06:22 +0300

    Create Tr_class.hpp

---
+++ 2_1_2c/Tr_class.hpp
@@ -0,0 +1,12 @@
+#ifndef __TR_CLASS__H
+#define __TR_CLASS__H
+class Tr_class
+{
+private:
+  int A,B,C;
+public:
+  Tr_class(int A, int B, int C);
+  int m_CalcPr();
+  float m_CalcVol();
+};
+#endif
