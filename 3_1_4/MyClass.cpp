595a7649cd911891154344a8f3b78477bd274f4a
st <150149808+codefishin@users.noreply.github.com>
2025-02-26 00:10:14 +0300

    Create MyClass.cpp

---
+++ 3_1_4/MyClass.cpp
@@ -0,0 +1,63 @@
+#include "MyClass.hpp"
+MyClass::MyClass()
+{
+  std::cout << "Default constructor\n";
+}
+MyClass::MyClass(int size)
+{
+  myarray = new int[size];
+  std::cout << "Constructor set\n";
+  this->size = size;
+}
+MyClass::MyClass(const MyClass& obj)
+{
+  std::cout << "Copy constructor\n";
+  size = obj.size;
+  myarray = new int[size];
+  for(int i = 0; i < size; i++) myarray[i] = obj.myarray[i];
+}
+MyClass::~MyClass()
+{
+  static int c = 0;
+  c += 1;
+  std::cout << "Destructor";
+  if(myarray != nullptr) delete[] myarray;
+  if (c < 3) std::cout << "\n"; // hack! Думал что ошибка в неверном выводе деструктора, так что сделал вот это. Потом понял что не выводил сумму массива
+}
+void MyClass::input()
+{
+  for(int i = 0; i < size; i++) std::cin >> myarray[i];
+}
+void MyClass::createArray()
+{
+  myarray = new int[size];
+}
+void MyClass::printArray()
+{
+  for (int i = 0; i < size; i++)
+  {
+    std::cout << myarray[i];
+    if (i < size - 1) std::cout << " ";
+  }
+  std::cout << "\n";
+}
+void MyClass::method1()
+{
+  for(int i = 0; i < size; i = i+2)
+  {
+    myarray[i] = myarray[i] + myarray[i+1];
+  }
+}
+void MyClass::method2()
+{
+  for(int i = 0; i < size; i = i+2)
+  {
+    myarray[i] = myarray[i] * myarray[i+1];
+  }
+}
+int MyClass::getSum()
+{
+  int temp = 0;
+  for(int i = 0; i < size; i++) temp = temp + myarray[i];
+  return temp;
+}
