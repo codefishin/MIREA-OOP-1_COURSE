762810a3cb964d8d429a382cdcb19bee82b200e6
st <150149808+codefishin@users.noreply.github.com>
2025-02-26 00:13:52 +0300

    Create MyClass.hpp

---
+++ 3_1_5/MyClass.hpp
@@ -0,0 +1,23 @@
+#ifndef __MYCLASS__H
+#define __MYCLASS__H
+#include <iostream>
+class MyClass
+{
+private:
+  int * myarray;
+  int size;
+public:
+  MyClass(); // Конструктор
+  MyClass(int size); // Параметризированный конструктор
+  MyClass(const MyClass & obj); // Конструктор копирования
+  ~MyClass(); // Деструктор
+  void input(); // Ввод. Мне не даёт отправить задание из-за неверной постановки переменной.
+  void createArray(); // Инициализация массива
+  void printArray(); // Вывод массива
+  int getSum(); // Метод возвращения общей суммы всех элементов массива
+  void method1(); // Метод 1
+  void method2(); // Метод 2
+  int * returnpointer();
+  void assignpointer(int * ptr);
+};
+#endif
